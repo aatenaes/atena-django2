1649eefcb84b27282693efb162bc4be6940607fd
aatenaes <atenaeshrattttt@gmail.com>
2023-10-26 18:06:17 +0330

    https://github.com/aatenaes/atena-django2

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import  Post,comment
+admin.site.register(Post)
+admin.site.register(comment)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title=models.CharField(max_length=20)
+    body=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    update_at=models.DateTimeField(auto_now=True)
+    def __str__(self):
+     return self.body[:50]
+class comment(models.Model):
+    post=models.ForeignKey(Post,on_delete=models.CASCADE)
+    body=models.TextField()
+    is_validate=models.BooleanField(default=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+    update_at=models.DateTimeField(auto_now=True)
+    def __str__(self):
+     return self.body[:50]
